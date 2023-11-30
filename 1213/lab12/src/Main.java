11f48657f13da8a89bf60c72a6df51bc72027cea
Monarch <trekman2409@gmail.com>
2023-11-30 13:53:27 -0500

    Fix: Misc
    Misc bugfixes and a larger example data pool

--- 1213/lab12/src/Main.java
+++ 1213/lab12/src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        Scanner fileScanner;// = new Scanner(new File("C:\\TNGRP\\Javaexercises\\1213\\lab12\\src\\inputData.txt"));
+        Scanner fileScanner;
         ArrayList<Student> allStudents = new ArrayList<Student>();
         ArrayList<Professor> allProfessors = new ArrayList<Professor>();
         try{
@@ -12,11 +12,8 @@ public class Main {
             String line;
             while(fileScanner.hasNext()){
                 line = fileScanner.nextLine();
-            
-            
-        
-        
             String[] allValues = line.split(",");
+            try{
             if(allValues[0].equalsIgnoreCase("Student")){
                 Student newStudent = new Student(allValues[1], allValues[2], Integer.parseInt(allValues[3]), allValues[4], Double.parseDouble(allValues[5]), Integer.parseInt(allValues[6]));
                 allStudents.add(newStudent);
@@ -25,35 +22,39 @@ public class Main {
                 Professor newProfessor = new Professor(allValues[1], allValues[2], Integer.parseInt(allValues[3]), allValues[4], Double.parseDouble(allValues[5]));
                 allProfessors.add(newProfessor);
             }
-            
+            }
+            catch(ArrayIndexOutOfBoundsException ex){
+                System.out.println("Caught ArrayIndexOutOfBoundsException when closing output stream, try again.");
+            }
         }
         fileScanner.close();
         }
             catch(FileNotFoundException ex){
             System.out.println(" Caught FileNotFoundException for inputData.txt. Try again making sure the file name and path are correct.");
-            
         }
         Collections.sort(allStudents);
         Collections.reverse(allStudents);
 
-        System.out.println(allStudents);
-        System.out.println(allProfessors);
+        //System.out.println(allStudents);
+        //System.out.println(allProfessors);
+
     try{
-        FileOutputStream fs = new FileOutputStream("C:\\TNGRP\\Javaexercises\\1213\\lab12\\src\\ScholarshipCandidates.txt");
+        FileOutputStream fs = new FileOutputStream("ScholarshipCandidates.txt");
         PrintWriter outFS = new PrintWriter(fs);
         outFS.println("Our top candidates for the scholarship are: ");
+        System.out.println("Our top candidates for the scholarship have been dumped into ScholarshipCandidates.txt");
     
         for(Student s : allStudents){
             if(s.getGPA() < 3.2){
-                System.out.println(s + " is ineligible");
+                //System.out.println(s + " is ineligible");
                 outFS.println(s + " is ineligible");
             }
             else if(s.getGPA() < 3.5 && s.getGPA() >= 3.2){
-                System.out.println(s + " is a middle candidate!");
+                //System.out.println(s + " is a middle candidate!");
                 outFS.println(s + "is a middle candidate!");
             }
             else{
-                System.out.println(s + " is a top candidate!");
+                //System.out.println(s + " is a top candidate!");
                 outFS.println(s + " is a top candidate!");
             }
         }
